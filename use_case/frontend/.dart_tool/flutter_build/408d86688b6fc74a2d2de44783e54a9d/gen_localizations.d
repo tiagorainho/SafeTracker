3b31887e0ed214cad93cfa39d9e2dfb396d87e4c
Calhau <ximaxerpt@gmail.com>
2023-04-15 17:54:20 +0100

    basic app
    app does nothing besides getting beacon info

---
+++ use_case/frontend/.dart_tool/flutter_build/408d86688b6fc74a2d2de44783e54a9d/gen_localizations.d
@@ -0,0 +1,1 @@
+ C:\\Users\\ximax\\Desktop\\shift\\fe_shift\\.dart_tool\\flutter_gen\\gen_l10n\\app_localizations_en.dart C:\\Users\\ximax\\Desktop\\shift\\fe_shift\\.dart_tool\\flutter_gen\\gen_l10n\\app_localizations.dart:  C:\\Users\\ximax\\Desktop\\shift\\fe_shift\\l10n.yaml C:\\Users\\ximax\\Desktop\\shift\\fe_shift\\lib/src/localization\\app_en.arb
---
+++ use_case/frontend/android/app/src/main/kotlin/com/example/fe_shift/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fe_shift
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
